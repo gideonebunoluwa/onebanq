3410059971c5f199c15b6c89dcbe6cc72dc00782
Plut0r <ogunolaololade@gmail.com>
2024-01-09 16:07:48 +0100

    feat: added terms of use content

---
+++ components/Terms-of-use/Content.tsx
@@ -0,0 +1,146 @@
+import React from "react";
+
+function Content() {
+  return (
+    <div className="mt-20 mb-24 w-[90%] mx-auto space-y-6">
+      <section className="space-y-3">
+        <h2 className="font-bold text-lg">1. INTRODUCTION</h2>
+        <p>
+          These Terms of Use (&ldquo;Terms&quot;) govern your access to and use
+          of the credit card services (&ldquo;Services&quot;) provided by
+          ONEBANQ Technologies (&ldquo;we&quot;, &ldquo;us&quot;,
+          &ldquo;our&quot;), a duly licensed digital lending platform in
+          Nigeria. By applying for or using a{" "}
+          <span className="font-bold">BanqCard</span> (&ldquo;Card&quot;), you
+          agree to be bound by these Terms.
+        </p>
+      </section>
+      <section className="space-y-3">
+        <h2 className="font-bold text-lg">2. ELIGIBILITY</h2>
+        <p>To be eligible for a Card, you must:</p>
+        <ul className="list-disc list-inside space-y-2">
+          <li>Be at least 18 years old;</li>
+          <li>Be a resident of Nigeria;</li>
+          <li>Have a valid and verifiable Bank Verification Number (BVN);</li>
+          <li>Have a verifiable source of income;</li>
+          <li>Meet our creditworthiness criteria.</li>
+        </ul>
+      </section>
+      <section className="space-y-3">
+        <h2 className="font-bold text-lg">3. APPLICATION AND ISSUANCE</h2>
+        <p>
+          You may apply for a Card by requesting from our mobile app. We reserve
+          the right to approve or deny your application at our sole discretion.
+          If your application is approved, we will issue you a Card and you will
+          be subject to these Terms.
+        </p>
+      </section>
+      <section className="space-y-3">
+        <h2 className="font-bold text-lg">4. FEES AND CHARGES</h2>
+        <p>
+          You will be subject to various fees and charges associated with your
+          Card, including:
+        </p>
+        <ul className="list-disc list-inside space-y-2">
+          <li>Annual fee;</li>
+          <li>Transaction fees;</li>
+          <li>Cash advance fees;</li>
+          <li>Late payment fees;</li>
+          <li>Over-the-limit fees; and</li>
+          <li>Foreign transaction fees.</li>
+        </ul>
+        <p>
+          A complete list of fees and charges will be provided to you in a
+          separate document or on our website.
+        </p>
+      </section>
+      <section className="space-y-3">
+        <h2 className="font-bold text-lg">
+          5. TRANSACTION FEES AND CREDIT LIMIT
+        </h2>
+        <p>
+          Your Card will have a variable transaction fee that may change based
+          on your creditworthiness and market conditions. We will determine your
+          initial credit limit based on your income, credit history, and other
+          factors. We may increase or decrease your credit limit at any time, at
+          our sole discretion.
+        </p>
+      </section>
+      <section className="space-y-3">
+        <h2 className="font-bold text-lg">6. PAYMENT AND BILLING</h2>
+        <p>
+          Your monthly statement will be available online and will show all
+          transactions made with your Card during the previous month. You are
+          responsible for making at least the minimum payment on your Card by
+          the due date each month. Failure to make timely payments may result in
+          late payment fees, interest charges, and other penalties.
+        </p>
+      </section>
+      <section className="space-y-3">
+        <h2 className="font-bold text-lg">7. USE OF CARD</h2>
+        <p>
+          You agree to use your Card only for authorized purposes and in
+          accordance with applicable laws and regulations. You may not use your
+          Card for any illegal or fraudulent activities. We reserve the right to
+          suspend or terminate your Card at any time, at our sole discretion,
+          for any reason or no reason.
+        </p>
+      </section>
+      <section className="space-y-3">
+        <h2 className="font-bold text-lg">8. LIABILITY AND SECURITY</h2>
+        <p>
+          You are responsible for safeguarding your Card and PIN. You are liable
+          for any unauthorized transactions made with your Card up to the amount
+          of your credit limit, unless you report the loss or theft of your Card
+          to us promptly. We are not liable for any damages you may suffer as a
+          result of unauthorized transactions, unless they are caused by our
+          negligence.
+        </p>
+      </section>
+      <section className="space-y-3">
+        <h2 className="font-bold text-lg">9. PRIVACY POLICY</h2>
+        <p>
+          Your Card will have a variable transaction fee that may change based
+          on your creditworthiness and market conditions. We will determine your
+          initial credit limit based on your income, credit history, and other
+          factors. We may increase or decrease your credit limit at any time, at
+          our sole discretion.We collect and use your personal information in
+          accordance with our Privacy Policy, which is available on our website.
+          The Privacy Policy describes the types of information we collect, how
+          we use it, and with whom we share it.
+        </p>
+      </section>
+      <section className="space-y-3">
+        <h2 className="font-bold text-lg">
+          10. GOVERNING LAW AND DISPUTE RESOLUTION
+        </h2>
+        <p>
+          These Terms shall be governed by and construed in accordance with the
+          laws of the Federal Republic of Nigeria. Any dispute arising out of or
+          in connection with these Terms shall be settled by binding arbitration
+          in accordance with the rules of the Arbitration and Conciliation Act,
+          Cap A18, Laws of the Federation of Nigeria, 2004.
+        </p>
+      </section>
+      <section className="space-y-3">
+        <h2 className="font-bold text-lg">11. MODIFICATIONS</h2>
+        <p>
+          We reserve the right to modify these Terms at any time without prior
+          notice. You will be notified of any changes by email or by posting the
+          updated Terms on our website. Your continued use of the Services after
+          the effective date of any changes constitutes your acceptance of the
+          new Terms.
+        </p>
+      </section>
+      <section className="space-y-3">
+        <h2 className="font-bold text-lg">12. ENTIRE AGREEMENT</h2>
+        <p>
+          These Terms constitute the entire agreement between you and us with
+          respect to the Services.
+        </p>
+      </section>
+    </div>
+  );
+}
+
+export default Content;
